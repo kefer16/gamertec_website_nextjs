969d8bca0a7345b5900747e41c28858b49aa17da
kefer <morafi.1999.16@gmail.com>
2024-06-19 00:09:35 -0500

    se cambia UI de Drawer Brand

--- src/app/brand/page.tsx
+++ src/app/brand/page.tsx
@@ -34,7 +34,7 @@ import {
    TableHeader,
    TableRow,
 } from "@/components/ui/table";
-import { useEffect, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { BrandApi } from "../api/brand/brand.api";
 import { BrandResponseDto } from "../api/brand/dto/responses/brand.response.dto";
 import {
@@ -56,7 +56,24 @@ import {
    FormLabel,
    FormMessage,
 } from "@/components/ui/form";
-import { useForm } from "react-hook-form";
+import { FormSubmitHandler, SubmitHandler, useForm } from "react-hook-form";
+import { Label } from "@/components/ui/label";
+import { Switch } from "@/components/ui/switch";
+import {
+   Popover,
+   PopoverContent,
+   PopoverTrigger,
+} from "@/components/ui/popover";
+import { Calendar } from "@/components/ui/calendar";
+import { format } from "date-fns";
+import {
+   CalendarPlus as CalendarPlusIcon,
+   CalendarClock as CalendarClockIcon,
+} from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { on } from "events";
+import { Separator } from "@/components/ui/separator";
 
 export const columns: ColumnDef<BrandResponseDto>[] = [
    {
@@ -171,6 +188,18 @@ export default function Page() {
    );
    const [rowSelection, setRowSelection] = useState({});
    const [data, setData] = useState<BrandResponseDto[]>([]);
+   const [shortName, setShortName] = useState<string>("");
+   const [fullName, setFullName] = useState<string>("");
+   const [creationDate, setCreationDate] = useState<Date>();
+   const [isActive, setIsActive] = useState<boolean>(false);
+
+   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      console.log("fecha creacion", creationDate);
+      console.log("short name", shortName);
+      console.log("full name", fullName);
+      console.log("is active", isActive);
+   };
 
    const getBrans = async () => {
       const apiBrand = new BrandApi();
@@ -188,9 +217,9 @@ export default function Page() {
 
    useEffect(() => {
       getBrans();
+      setCreationDate(new Date());
    }, []);
 
-   const onSubmit = () => {};
    const table = useReactTable({
       data,
       columns,
@@ -209,7 +238,7 @@ export default function Page() {
          rowSelection,
       },
    });
-   const form = useForm();
+
    return (
       <div className="flex flex-col">
          <div className="w-full max-w-[1440px] mx-auto">
@@ -346,45 +375,77 @@ export default function Page() {
                <Button variant="outline">Open Drawer</Button>
             </DrawerTrigger>
             <DrawerContent>
-               <div className="mx-auto w-full max-w-sm">
-                  <DrawerHeader>
-                     <DrawerTitle>Move Goal</DrawerTitle>
-                     <DrawerDescription>
-                        Set your daily activity goal.
-                     </DrawerDescription>
-                  </DrawerHeader>
-                  <div className="p-4 pb-0">
-                     <Form {...form}>
-                        <form
-                           onSubmit={form.handleSubmit(onSubmit)}
-                           className="space-y-8"
-                        >
-                           <FormField
-                              control={form.control}
-                              name="username"
-                              render={({ field }) => (
-                                 <FormItem>
-                                    <FormLabel>Username</FormLabel>
-                                    <FormControl>
-                                       <Input placeholder="shadcn" {...field} />
-                                    </FormControl>
-                                    <FormDescription>
-                                       This is your public display name.
-                                    </FormDescription>
-                                    <FormMessage />
-                                 </FormItem>
-                              )}
+               <div className="p-8">
+                  <form
+                     onSubmit={(event) => onSubmit(event)}
+                     // className="space-y-2"
+                  >
+                     <DrawerHeader className="px-0">
+                        <div className="flex justify-between gap-2">
+                           <DrawerTitle>Marca de productos</DrawerTitle>
+                           <Switch
+                              onCheckedChange={setIsActive}
+                              checked={isActive}
                            />
-                           <Button type="submit">Submit</Button>
-                        </form>
-                     </Form>
-                  </div>
-                  <DrawerFooter>
-                     <Button>Guardar</Button>
-                     <DrawerClose asChild>
-                        <Button variant="outline">Cancel</Button>
-                     </DrawerClose>
-                  </DrawerFooter>
+                        </div>
+
+                        <DrawerDescription>
+                           Ingrese la marca para los productos
+                        </DrawerDescription>
+                     </DrawerHeader>
+                     <div className="flex h-5 justify-between items-center gap-4">
+                        <Button
+                           disabled
+                           variant={"outline"}
+                           className={cn(
+                              "w-full justify-start text-left font-normal",
+                              !creationDate && "text-muted-foreground"
+                           )}
+                        >
+                           <CalendarPlusIcon className="mr-2 h-4 w-4" />
+                           {creationDate
+                              ? format(creationDate, "dd-MM-yyyy HH:mm:ss")
+                              : ""}
+                        </Button>
+                        <Separator orientation="vertical" />
+                        <Button
+                           disabled
+                           variant={"outline"}
+                           className={cn(
+                              "w-full justify-start text-left font-normal",
+                              !creationDate && "text-muted-foreground"
+                           )}
+                        >
+                           <CalendarClockIcon className="mr-2 h-4 w-4" />
+                           {creationDate
+                              ? format(creationDate, "dd-MM-yyyy HH:mm:ss")
+                              : ""}
+                        </Button>
+                     </div>
+                     <div className="py-4">
+                        <div>
+                           <Label htmlFor="short_name">Nombre Corto</Label>
+                           <Input
+                              onChange={(e) => setShortName(e.target.value)}
+                              value={shortName}
+                           />
+                        </div>
+
+                        <div>
+                           <Label>Nombre Completo</Label>
+                           <Input
+                              onChange={(e) => setFullName(e.target.value)}
+                              value={fullName}
+                           />
+                        </div>
+                     </div>
+                     <DrawerFooter className="px-0">
+                        <Button type="submit">Guardar</Button>
+                        <DrawerClose asChild>
+                           <Button variant="outline">Cancel</Button>
+                        </DrawerClose>
+                     </DrawerFooter>
+                  </form>
                </div>
             </DrawerContent>
          </Drawer>
